33a26517c3ba8879bf54c030d0b88910001b44dd
pasted0 <pastedv1ontop@gmail.com>
2025-03-09 21:05:43 -0500

    Rewrote main.lua
    Overhauled notification system,
    Changed the way that commands functions allowing me to make modules more easily,
    Added chat command,
    Added say command
    Fixed prefix command
    And so much more
    Most of these changes will take affect tomorrow since I have them implemented somewhere but didn't save the moduels

--- main.lua
+++ main.lua
@@ -1,94 +1,153 @@
+repeat task.wait() until game:IsLoaded()
 -->> VARIABLES <<--
 local textChatService = cloneref(game:GetService("TextChatService"))
 local tweenService = cloneref(game:GetService("TweenService"))
 local playersService = cloneref(game:FindService("Players"))
 local CoreGui = cloneref(game:GetService("CoreGui"))
 local lplr = playersService.LocalPlayer
-local commandsFolder = Instance.new("Folder", textChatService)
-local commandPrefix = ";"
+local whitelist = {}
+local prefix = ";"
 
-local commands = {}
+local commands = {
+    ["rejoin"] = function()
+      notif("Rejoin:","Rejoining Server", 3)
+      cloneref(game:GetService("TeleportService")):Teleport(game.PlaceId, lplr)
+
+    end,
+    
+    ["say"] = function(args)
+        table.remove(args, 1)
+        task.wait()
+        chat(table.concat(args, " "))
+    end,
+    
+    ["reset"] = function()
+        lplr.Character:BreakJoints()
+    end,
+    
+    ["speed"] = function(args)
+        table.remove(args, 1)
+        task.wait()
+        local ws = tonumber(args[1])
+        if ws and ws >= 0 then
+            lplr.Character:WaitForChild("Humanoid").WalkSpeed = ws
+        else
+            notif("Inquire", "Please enter a valid number", 5)
+        end
+    end,
+    ["whitelist"] = function(args)
+      table.remove(args, 1)
+      task.wait()
+      local player = playersService:FindFirstChild(args[1])
+      if player then
+        table.insert(whitelist, player.Name)
+        notif("Success!", player.DisplayName.."(@"..player.Name..") Has Been Whitelisted", 10)
+      end
+    end,
+    
+    ["unwhitelist"] = function(args)
+      table.remove(args, 1)
+      task.wait()
+      if table.find(whitelist, args[1]) then
+        table.remove(whitelist, args[1])
+        else
+          notif("UnWhitelist", args[1] .. " Isnt Whitelisted or Isnt in you're game")
+      end
+    end,
+    
+    ["prefix"] = function(args)
+        table.remove(args, 1)
+        task.wait()
+        prefix = args[1]
+        notif("Prefix:", "Prefix changed to " .. args[1], 6)
+    end
+}
+task.wait()
+commands["ws"] = commands.speed
+commands["walkspeed"] = commands.speed
+commands["rj"] = commands.rejoin
+commands["re"] = commands.reset
+commands["wl"] = commands.whitelist
+commands["unwl"] = commands.unwhitelist
+commands["uwl"] = commands.unwhitelist
 -->> FUNCTIONS <<--
+function chat(msg)
+    if textChatService.ChatInputBarConfiguration.TargetTextChannel then
+        textChatService.ChatInputBarConfiguration.TargetTextChannel:SendAsync(msg)
+    else
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All")
+    end
+end
+
+
+
 function notif(title, text, duration)
-    local ScreenGui = Instance.new("ScreenGui", CoreGui)
-    ScreenGui.Name = "G7°£=§`π÷~HS6-86202828-$&#-(2:"
+    local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
+    ScreenGui.Name = "G7Â°Â£=Â§`Ï€Ã·~HS6-86202828-$&#-(2:"
     ScreenGui.ResetOnSpawn = false
     ScreenGui.Enabled = true
+
     local main = Instance.new("Frame", ScreenGui)
     main.Name = "Main"
-    main.Size = UDim2.new(0.25, 0, 0.2, 0)
+    main.Size = UDim2.new(0.25, 0, 0.12, 0)
     main.Position = UDim2.new(1, 0, 0.55, 0)
+    main.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+    main.BackgroundTransparency = 0.2
+    main.BorderSizePixel = 0
+
+    local corner = Instance.new("UICorner", main)
+    corner.CornerRadius = UDim.new(0, 8)
 
     local titleLabel = Instance.new("TextLabel", main)
-    titleLabel.TextWrapped = false
-    titleLabel.Size = UDim2.new(1, 0, 1, 0)
-    titleLabel.TextXAlignment = Enum.TextXAlignment.Center
+    titleLabel.Size = UDim2.new(1, -10, 0.4, 0)
+    titleLabel.Position = UDim2.new(0, 5, 0, 0)
+    titleLabel.TextXAlignment = Enum.TextXAlignment.Left
     titleLabel.TextYAlignment = Enum.TextYAlignment.Top
     titleLabel.TextScaled = true
+    titleLabel.BackgroundTransparency = 1
+    titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    titleLabel.Text = title
 
-    titleLabel.Text = [[
-]] .. title .. "\n" .. text
+    local textLabel = Instance.new("TextLabel", main)
+    textLabel.Size = UDim2.new(1, -10, 0.6, 0)
+    textLabel.Position = UDim2.new(0, 5, 0.4, 0)
+    textLabel.TextXAlignment = Enum.TextXAlignment.Left
+    textLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+    textLabel.TextScaled = true
+    textLabel.BackgroundTransparency = 1
+    textLabel.TextColor3 = Color3.fromRGB(200, 200, 200)
+    textLabel.Text = text
 
-    local notificationTweenInfo = TweenInfo.new(
-        0.5,
-        Enum.EasingStyle.Linear,
-        Enum.EasingDirection.In,
-        0,
-        false
-    )
-    tweenService:create(main, notificationTweenInfo, { Position = UDim2.new(0.75, 0, 0.55, 0) }):Play()
+    local progressBar = Instance.new("Frame", main)
+    progressBar.Size = UDim2.new(1, 0, 0.05, 0)
+    progressBar.Position = UDim2.new(0, 0, 0.95, 0)
+    progressBar.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    progressBar.BorderSizePixel = 0
+
+    local progressCorner = Instance.new("UICorner", progressBar)
+    progressCorner.CornerRadius = UDim.new(0, 4)
+
+    local progressTweenInfo = TweenInfo.new(duration, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+    tweenService:Create(progressBar, progressTweenInfo, { Size = UDim2.new(0, 0, 0.05, 0) }):Play()
+
+    local notificationTweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false)
+    tweenService:Create(main, notificationTweenInfo, { Position = UDim2.new(0.75, 0, 0.55, 0) }):Play()
+
     task.wait(duration)
-    tweenService:create(main, notificationTweenInfo, { Position = UDim2.new(1, 0, 0.55, 0) }):Play()
+
+    tweenService:Create(main, notificationTweenInfo, { Position = UDim2.new(1, 0, 0.55, 0) }):Play()
 end
 
-function createCommand(commandName, commandAlias, callbackFunc)
-    local newCommand = Instance.new("TextChatCommand", commandsFolder)
-    newCommand.Name = commandName
-    newCommand.PrimaryAlias = commandAlias
-    newCommand.Parent = textChatService
-
-    commands[commandName] = false
-
-    newCommand.Triggered:Connect(function()
-        commands[commandName] = not commands[commandName]
-        callbackFunc()
-    end)
-end
-
-lplr.Chatted:Connect(function(message)
-    if message:sub(1, #commandPrefix) == commandPrefix then
-        local commandAndArguments = message:sub(#commandPrefix + 1)
-        local commandName = commandAndArguments:match("^[%w_]+")
-        local commandArguments = commandAndArguments:sub(#commandName + 2)
-        
-        if commands[commandName] then
-            commands[commandName](commandArguments)
+lplr.Chatted:Connect(function(msg)
+    if string.sub(msg, 1, #prefix) == prefix then
+        local args = msg:split(" ")
+        local cmd = string.sub(args[1], #prefix + 1)
+        if commands[cmd:lower()] then
+            commands[cmd:lower()](args)
         end
     end
 end)
 
--->> COMMANDS <<--
-createCommand("Prefix", commandPrefix .. "prefix", function(commandArguments)
-    if commandArguments and #commandArguments > 0 then
-        commandPrefix = commandArguments:match("^%s*(.-)%s*$")
-        print("Prefix changed to: " .. commandPrefix)
-    else
-        print("No new prefix provided.")
-    end
-end)
-
-createCommand("Reset", commandPrefix .. "reset", function()
-    if commands["Reset"] then
-        lplr.Character:BreakJoints()
-    end
-end)
-
-createCommand("Test", commandPrefix .. "test", function()
-    if commands["Test"] then
-        lplr.Character.Humanoid.WalkSpeed = 90
-    else
-        lplr.Character.Humanoid.WalkSpeed = 16
-    end
-end)
 
 notif("Inquire", "Inquire Has Loaded!", 5)
+

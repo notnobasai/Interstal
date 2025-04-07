919c33020e40fbec8412d5efc37ac5cf43bf43bf
pasted0 <pastedv1ontop@gmail.com>
2025-04-07 12:22:07 -0500

    Update EntityLibrary.lua

--- EntityLibrary.lua
+++ EntityLibrary.lua
@@ -9,16 +9,17 @@ getgenv().EntityLibrary = {
   ["Players"] = {},
   ["Characters"] = {},
   ["NPCs"] = {},
-  ["lplr"] = lplr
+  ["LocalPlayer"] = lplr and lplr.Character or nil
 }
-while task.wait(15) do
-for _, Characters in next, game.Workspace:GetChildren() do
-  if Characters:FindFirstChildWhichIsA("Humanoid") and Characters:IsA("Model") then
-    if playerService:GetPlayerFromCharacter(Characters) ~= nil then
-      table.insert(getgenv().EntityLibrary.Characters, Characters)
-    else
-      table.insert(getgenv().EntityLibrary.NPCs, Characters)
+
+while task.wait(1) do
+  for _, Characters in next, game.Workspace:GetChildren() do
+    if Characters:FindFirstChildWhichIsA("Humanoid") and Characters:IsA("Model") then
+      if playerService:GetPlayerFromCharacter(Characters) ~= nil then
+        table.insert(getgenv().EntityLibrary.Characters, Characters)
+        else
+          table.insert(getgenv().EntityLibrary.NPCs, Characters)
+      end
     end
   end
-end
 end

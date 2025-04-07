91e8b29f83acba2eee103d56d3c5586537ba4a9e
pasted0 <pastedv1ontop@gmail.com>
2025-04-07 02:19:22 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 getgenv().InstalledStatus = getgenv().InstalledStatus or "Not Tested"
 
 if getgenv().InstalledStatus == "Installed" then
-  loadstring(readfile("Inquire/EntityLibrary.lua"))()
-  loadstring(readfile("Inquire/main.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
+  
 elseif getgenv().InstalledStatus == "Not Tested" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/installer.lua"))()
+  
 elseif InstalledStatus == "Installer.lua" then
-  loadstring(readfile("Inquire/EntityLibrary.lua"))()
-  loadstring(readfile("Inquire/main.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
 end

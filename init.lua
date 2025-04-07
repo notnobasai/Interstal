4a01e808fc06d8ef4445c5f3d7d0e577d0710ae6
pasted0 <pastedv1ontop@gmail.com>
2025-04-07 13:34:08 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,22 @@
+
 getgenv().InstalledStatus = getgenv().InstalledStatus or "Not Tested"
 
 if getgenv().InstalledStatus == "Installed" then
-  task.spawn(function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))() 
-    task.spawn(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
-    end)
-  end)
-elseif getgenv().InstalledStatus == "Not Tested" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/installer.lua"))()
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
+end)
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))()
 
-elseif InstalledStatus == "Installer.lua" then
-  task.spawn(function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))() 
-    task.spawn(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
-    end)
-  end)
+end)
+elseif getgenv().InstalledStatus == "Not Tested" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/installer.lua"))()
+elseif getgenv().InstalledStatus == "Installer.lua" then
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/main.lua"))()
+end)
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/EntityLibrary.lua"))()
+end)
 end
+

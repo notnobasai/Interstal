cd72a95af2b3ad9c1774b9991334c05b7373be01
pasted0 <pastedv1ontop@gmail.com>
2025-04-07 01:02:42 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,11 @@
 getgenv().InstalledStatus = getgenv().InstalledStatus or "Not Tested"
+
 if getgenv().InstalledStatus == "Installed" then
-  task.spawn(function()
-    loadstring(readfile("Inquire/EntityLibrary.lua"))
-  end)
-  task.wait()
+  loadstring(readfile("Inquire/EntityLibrary.lua"))()
   loadstring(readfile("Inquire/main.lua"))()
 elseif getgenv().InstalledStatus == "Not Tested" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/pasted0/Inquire/refs/heads/main/installer.lua"))()
-  elseif InstalledStatus == "Installer.lua" then
-     task.spawn(function()
-       loadstring(readfile("Inquire/EntityLibrary.lua"))
-     end)
-     task.wait()
-     loadstring(readfile("Inquire/main.lua"))()
+elseif InstalledStatus == "Installer.lua" then
+  loadstring(readfile("Inquire/EntityLibrary.lua"))()
+  loadstring(readfile("Inquire/main.lua"))()
 end

49ecfede61f1f688e09920a3a1ffd7034e10d98b
pasted0 <pastedv1ontop@gmail.com>
2025-04-06 23:32:38 -0500

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,6 +1,7 @@
 local http_request = syn and syn.request or http and http.request or http_request or httprequest
 getgenv().InstalledStatus = true
 local url = "https://api.github.com/repos/pasted0/%s/contents/%s"
+
 local function getFile(Repo, FileName)
   local options = {
     Method = 'GET',
@@ -26,53 +27,47 @@ local isfolder = isfolder or function(folder)
     end
 end
 
-
 if not isfolder("Inquire") then
   getgenv().InstalledStatus = false
   makefolder("Inquire")
 end
 
-
 if not isfile("Inquire/main.lua") then
   getgenv().InstalledStatus = false
   local main = getFile("Inquire/main.lua")
   if main and main.Body then
-    writefile("Inquire/main.lua" .. main.Body)
+    writefile("Inquire/main.lua", main.Body)
   end
 end
-
 
 if not isfile("Inquire/installer.lua") then
   getgenv().InstalledStatus = false
   local installer = getFile("Inquire/installer.lua")
   if installer and installer.Body then
-    writefile("Inquire/init.lua" .. installer.Body)
+    writefile("Inquire/installer.lua", installer.Body)
   end
 end
-
 
 if not isfile("Inquire/init.lua") then
   getgenv().InstalledStatus = false
   local init = getFile("Inquire/init.lua")
   if init and init.Body then
-    writefile("Inquire/init.lua" .. init.Body)
+    writefile("Inquire/init.lua", init.Body)
   end
 end
-
 
 if not isfile("Inquire/EntityLibrary.lua") then
   getgenv().InstalledStatus = false
-  local EntityLibrary = getFile("Inquire/main.lua")
+  local EntityLibrary = getFile("Inquire/EntityLibrary.lua")
   if EntityLibrary and EntityLibrary.Body then
-    writefile("Inquire/EntityLibrary.lua" .. EntityLibrary.Body)
+    writefile("Inquire/EntityLibrary.lua", EntityLibrary.Body)
   end
 end
-
 
 if getgenv().InstalledStatus == true then
   getgenv().InstalledStatus = "installed"
   task.wait()
-  loadstring(getFile("Inquire/init.lua"))
-  else
-    getgenv().InstalledStatus = "Installer.lua"
+  loadstring(getFile("Inquire/init.lua").Body)()
+else
+  getgenv().InstalledStatus = "Installer.lua"
 end
